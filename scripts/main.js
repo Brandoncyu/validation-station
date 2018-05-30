9d877210b9c0e3f1d630b41f51d0f153e6fb5c92
Brandon <brandoncyu@gmail.com>
2018-05-30 11:55:42 -0700

    hope this works

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
 console.log('Welcome to Validation Station!')
-console.log('NEW TRAIN CAR FORM:', document.querySelector('#new-train-car-form'))
+
+const submitButton = document.querySelector('#new-train-car-form')
+const list = document.querySelector('#car-list')
+
+
+let count = 0
+submitButton.addEventListener('submit', (event) => {
+
+  event.preventDefault()
+  // const kind = document.querySelector('#train-kind')
+  const kind = event.target.kind
+  const seats = event.target.seats
+  const brand = event.target.brand
+  if ((kind.value === 'observation' || kind.value === 'passenger') && !seats.value){
+    const error = document.querySelector('#error')
+    error.querySelector('p').textContent = 'You must have a number of seats'
+    error.classList.remove("d-none")
+  } else{
+    count++
+    const li = document.createElement('li')
+    li.textContent = `Car #${count} is a ${kind.value} car, brand ${brand.value}.`
+    if (seats.value) li.textContent+= ` It seats ${seats.value} people.`
+    list.append(li)
+    error.querySelector('p').textContent = 'You added a car!'
+    error.classList.remove('d-none', 'alert-danger')
+    error.classList.add('alert-success')
+    seats.value = ''
+    kind.value = ''
+    brand.value = ''
+  }
+})
